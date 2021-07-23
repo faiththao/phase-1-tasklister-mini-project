5a0d56a04bd8814f983a0e1048bb467e3322ac9c
Faith <thao.faith2@gmail.com>
2021-07-23 13:52:36 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li');
+  let buttn = document.createElement('button');
+  buttn.addEventListener('click', handleDelete)
+  buttn.textContent = 'x';
+  li.textContent = `${todo}`;
+  li.appendChild(buttn);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
